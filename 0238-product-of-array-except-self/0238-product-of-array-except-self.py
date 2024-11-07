4ef290a6b7fbfdc428168aeff96277cf0bdb949f
Prakash Raj S <114377715+prakashRajs2709@users.noreply.github.com>
2024-11-07 09:23:31 +0530

    Time: 27 ms (49.42%), Space: 25.9 MB (34.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,15 +2,15 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         from itertools import accumulate 
         import operator
-        result = nums.copy()
+        result  = []
         prefix = list(accumulate(nums,operator.mul))
         suffix = list(accumulate(nums[::-1],operator.mul))[::-1]
 
         for i in range(len(nums)):
             if i==0:
-                result[i] = suffix[i+1]
+                result.append(suffix[i+1])
             elif i==len(nums)-1:
-                result[i] = prefix[i-1]
+                result.append(prefix[i-1])
             else:
-                result[i] = prefix[i-1]*suffix[i+1]
+                result.append(prefix[i-1]*suffix[i+1])
         return result
